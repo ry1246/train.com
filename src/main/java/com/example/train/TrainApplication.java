f56c6c83977654cbcab958915e459e09bfaf3648
r_hara <toooooom720@gmail.com>
2022-05-29 22:30:20 +0900

    3days

--- src/main/java/com/example/train/TrainApplication.java
+++ src/main/java/com/example/train/TrainApplication.java
@@ -2,6 +2,7 @@ package com.example.train;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TrainApplication {
