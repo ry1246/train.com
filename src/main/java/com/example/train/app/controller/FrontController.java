5d208098bd6003a256e4dbc464cbd22a404b0625
ry1246 <toooooom720@gmail.com>
2022-06-10 17:28:05 +0900

    fix: directory structure

--- src/main/java/com/example/train/app/controller/FrontController.java
+++ src/main/java/com/example/train/app/controller/FrontController.java
@@ -1,4 +1,4 @@
-package com.example.train.web;
+package com.example.train.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
