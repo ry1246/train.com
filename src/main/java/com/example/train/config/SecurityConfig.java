00c45e353b51f33af9517f9ed21d85da997b8db9
ry1246 <toooooom720@gmail.com>
2022-06-21 20:40:57 +0900

    fix

--- src/main/java/com/example/train/config/SecurityConfig.java
+++ src/main/java/com/example/train/config/SecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -14,20 +18,20 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.formLogin(login -> login
                 .loginProcessingUrl("/login")
-                .loginPage("/login")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login?error")
+                .loginPage("/loginForm")
+//                .defaultSuccessUrl("/")
+                .failureUrl("/loginForm?error")
                 .permitAll()
-                ).logout(logout -> logout
-                        .logoutSuccessUrl("/")
-                ).authorizeRequests(authz -> authz
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .mvcMatchers("/").permitAll()
-                        .mvcMatchers("/general").hasRole("GENERAL") // この行は必要なのか要調査
-                        .mvcMatchers("/admin").hasRole("ADMIN") // この行は必要なのか要調査
-                        
-                        .antMatchers("/h2-console/**").permitAll() // /h2-consoleにログイン無しで入れるように(MySQL実装の際に削除)
-                        .anyRequest().authenticated()
+                )
+            .logout(logout -> logout
+                .logoutSuccessUrl("/")
+                )
+            .authorizeRequests(auth -> auth
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .mvcMatchers("/").permitAll()
+
+                .antMatchers("/h2-console/**").permitAll() // /h2-consoleにログイン無しで入れるように(MySQL実装の際に削除)
+                .anyRequest().authenticated()
                 )
                 .csrf().ignoringAntMatchers("/h2-console/**"); // h2-consoleのCSRF対策(この行がないとConnect押下後ブロックされる)
                 http.headers().frameOptions().disable(); // h2-consoleへのlocalhostからの接続を許可するため
---
+++ src/main/java/com/example/train/repository/UserRepository.java
@@ -0,0 +1,13 @@
+// // Repositoryはおそらく不要のため、必要になるまでコメントアウト
+
+// package com.example.train.repository;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import com.example.train.model.UserEntity;
+
+// public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+//   public UserEntity findOneByUsername(String username);
+
+// }
